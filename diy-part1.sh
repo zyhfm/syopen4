504bdc11ff6e098e064fdd1cf1bb77f658365581
zyhfm <zenyahui@163.com>
2021-02-25 14:23:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,33 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+# 使用源码自带ShadowSocksR Plus+出国软件
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+
+# lede源码增加ShadowSocksR Plus+
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# lede源码增加passwall,也适用于增加其他插件的方法
+#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+
+#添加自定义插件链接（自己想要什么就github里面搜索然后添加）
+#git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/lean/luci-theme-edge  #主题-edge-动态登陆界面
+#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/lean/luci-theme-infinityfreedom  #透明主题
+#git clone -b master https://github.com/vernesong/OpenClash.git package/lean/luci-app-openclash  #openclash出国软件
+#git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash  #clash出国软件
+#git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan  #微信推送
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
+#git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
+#git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
+#svn co https://github.com/xiaorouji/openwrt-passwall/tree/main/luci-app-passwall package/lean/luci-app-passwall  #passwall出国软件
+#svn co https://github.com/xiaorouji/packages package/lean/package  #passwall出国软件配套
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#git clone https://github.com/281677160/openwrt-package.git package/openwrt-packages
+#git clone https://github.com/zyhfm/small.git package/openwrt-small
+#git clone https://github.com/kenzok8/small.git package/openwrt-small
+git clone https://github.com/281677160/openwrt-package.git package/openwrt-package
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
+git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
