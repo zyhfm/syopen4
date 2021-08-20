c5d9a59862cdc58b5a2e7559aa3374c32295ecb9
zyhfm <zenyahui@163.com>
2021-08-20 11:41:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.8/g' package/base-files/files/bin/config_generate
 
 # 替换https-dns-proxy.init文件,解决用LEDE源码加入passwall编译固件后DNS转发127.0.0.1#5053和12.0.0.1#5054问题
 #curl -fsSL  https://raw.githubusercontent.com/Lienol/openwrt-packages/19.07/net/https-dns-proxy/files/https-dns-proxy.init > feeds/packages/net/https-dns-proxy/files/https-dns-proxy.init
